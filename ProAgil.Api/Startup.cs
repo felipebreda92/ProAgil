a413575000697cb5189ae81e9acb28f21b0c3149
Felipe Tadeu Guerra Breda <felipe.breda@ppi-multitask.com.br>
2019-12-26 20:24:49 -0300

    Adicionado chamada Http no Angular e CORS na Api

--- ProAgil.Api/Startup.cs
+++ ProAgil.Api/Startup.cs
@@ -25,6 +25,7 @@ namespace ProAgil.Api
             });
                                                     
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,9 @@ namespace ProAgil.Api
             }
 
             app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin()
+                              .AllowAnyMethod()
+                              .AllowAnyHeader());
             app.UseMvc();
         }
     }
